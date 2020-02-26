ceeb2f622a2eb1ea83fd58ca35dcb0fe7e49e2a0
Duncan Uruchima <duruchima1@gmail.com>
2020-02-26 19:13:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,9 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  hash = {:key => "#{value}"}# return a hash that includes the key and value parameters passed into this method
+  key1=key
+  value1=value
+  hash = {:key1 => value1}# return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
